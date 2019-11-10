08cdce54700e533571dd98fcea95b8f891860821
Karunya Sethuraman <karunyas@mit.edu>
2019-11-10 03:53:17 -0500

    more data files to test with and sentiment analysis on lyrics

---
+++ lyrics_sentiment.py
@@ -0,0 +1,42 @@
+## take in the lyrics
+## write out to CSV file with sentiment, and probability values 
+## song_id | (pos/neg) | pos_prob | neg_prob
+
+import pandas as pd 
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+
+
+def get_song_feelings(song_id, song_lyric):
+	# for each row / song, grab id and lyrics
+	if not isinstance(song_lyric, float):
+		song_sentiment = TextBlob(song_lyric, analyzer=NaiveBayesAnalyzer()).sentiment
+		song_feeling = song_sentiment[0];
+		song_pos_prob = song_sentiment[1];
+		song_neg_prob = song_sentiment[2];
+		# print("hello")
+		add_song_to_sentiment_list(song_id, song_feeling)
+		return song_feeling
+	add_song_to_sentiment_list(song_id, "pos")
+	return "pos"
+
+def add_song_to_sentiment_list(song_id, song_feeling):
+	songs_by_sentiment[song_feeling].append(song_id)
+
+
+songs = pd.read_csv("data/top2018_lyrics.csv")
+songs_by_sentiment = {"pos": [], "neg": []}
+
+songs["feelings"] = songs.apply(lambda row: get_song_feelings(row["id"], row["lyrics"]), axis=1)
+
+## create a dict to csv file of song id's and their sentiment
+print(songs_by_sentiment)
+df_pos = pd.DataFrame(songs_by_sentiment["pos"])
+df_neg = pd.DataFrame(songs_by_sentiment["neg"])
+
+# print(df_pos)
+# pritn(df_neg)
+df_pos.to_csv("data/songs_by_sentiment_pos.csv")
+df_neg.to_csv("data/songs_by_sentiment_neg.csv")
+songs.to_csv("data/top2018_lyrics_feelings.csv")
+
