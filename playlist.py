4ad0aa3e46201dff05d9f220c4dddfc10f7e1a66
Claire Hsu <clhsu@mit.edu>
2019-11-09 22:15:44 -0500

    fixing image upload

--- playlist.py
+++ playlist.py
@@ -18,7 +18,7 @@ def retrieve_client_info():
 def retrieve_spotify_token():
     client_id, client_secret = retrieve_client_info()
     # print(client_id, client_secret)
-    token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public', client_id=client_id, client_secret=client_secret, redirect_uri='https://localhost:8080')
+    token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public,ugc-image-upload', client_id=client_id, client_secret=client_secret, redirect_uri='https://localhost:8080')
     # print("TOKEN: " , token)
     return token
 
@@ -32,7 +32,6 @@ def retrieve_already_token():
 # main_label is a string
 # returns: URL of playlist
 def make_playlist(filename, main_label, tracks):
-    filename = filename.split(".")
     token = retrieve_spotify_token()
     sp = spotipy.Spotify(auth=token)
     # sp = spotipy.Spotify(auth=retrieve_already_token())
@@ -40,7 +39,6 @@ def make_playlist(filename, main_label, tracks):
     r = random.randint(0,1000)
     playlist = sp.user_playlist_create(username, main_label + "_" + str(r))
     sp.user_playlist_add_tracks(username, playlist["id"], tracks)
-    return playlist["external_urls"]['spotify']
 
     # resize the image and encode
     basewidth = 300
@@ -50,21 +48,16 @@ def make_playlist(filename, main_label, tracks):
     img = img.resize((basewidth,hsize), Image.ANTIALIAS)
     img.save(filename) 
 
-    jpg_text_encoded = 'jpg1_b64 = \\\n"""' + base64.encodestring(open(filename,"rb").read()) + '"""'
+    with open(filename, "rb") as image_file:
+        jpg_text_encoded = base64.b64encode(image_file.read())
+        # print(jpg_text_encoded)
 
-    # add original image as playlist cover image
-    endpoint_url = "https://api.spotify.com/v1/playlists/{playlist_id}/images"
-    request_body = json.dumps({
-              "playlist_id": playlist["id"],
-              "Content-Type": jpg_text_encoded
-              "public": False
-            })
-
-    response = requests.put(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
-                            "Authorization":f"Bearer {token}"})
-    print(response.status_code)
+        # add original image as playlist cover image
+        endpoint_url = "https://api.spotify.com/v1/playlists/" + playlist["id"] + "/images"
+        response = requests.put(endpoint_url, data=jpg_text_encoded, headers={"Authorization":"Bearer {}".format(token), "Content-Type":"image/jpeg"})
+    return playlist["external_urls"]['spotify']
 
 
 if __name__ == "__main__":
-    url = make_playlist("bright", "", ['6DCZcSspjsKoFjzjrWoCdn', '6V1bu6o1Yo5ZXnsCJU8Ovk', '76cy1WJvNGJTj78UqeA5zr', '1rfofaqEpACxVEHIZBJe6W', '3swc6WTsr7rl9DqQKQA55C'])
+    url = make_playlist("data/bright.jpg", "bright", ['6DCZcSspjsKoFjzjrWoCdn', '6V1bu6o1Yo5ZXnsCJU8Ovk', '76cy1WJvNGJTj78UqeA5zr', '1rfofaqEpACxVEHIZBJe6W', '3swc6WTsr7rl9DqQKQA55C'])
     print(url)
